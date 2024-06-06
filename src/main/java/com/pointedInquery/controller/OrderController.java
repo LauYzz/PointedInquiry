fab80a1addbd5dedfd21331fc1c7a9145a57ac0a
Joye <2322009829@qq.com>
2024-06-06 12:00:00 -0500

    订单管理

--- src/main/java/com/pointedInquery/controller/OrderController.java
+++ src/main/java/com/pointedInquery/controller/OrderController.java
@@ -1,7 +1,5 @@
 package com.pointedInquery.controller;
 
-
-
 import com.pointedInquery.dto.OrderCreateDto;
 import com.pointedInquery.dto.OrderDetailedInfoDto;
 import com.pointedInquery.entity.Order;
@@ -11,11 +9,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-
+//from joye
 @RestController
 @RequestMapping("/order")
 public class OrderController {
-
     @Autowired
     private OrderService orderService;
 
--- src/main/java/com/pointedInquery/mapper/OrderMapper.java
+++ src/main/java/com/pointedInquery/mapper/OrderMapper.java
@@ -1,17 +1,18 @@
 package com.pointedInquery.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.pointedInquery.entity.Order;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface OrderMapper extends BaseMapper<Order> {
 
     @Update("update `order` set state=#{newState} where order_id=#{orderId}")
-    boolean updateOrderStatus(String newState, String orderId);
+    public boolean updateOrderStatus(String newState,String orderId);
 
     @Update("UPDATE `order` SET state='已取消' WHERE order_id=#{orderId}")
-    int cancleOrder(String orderId);
+    public int cancleOrder(String orderId);
 
 }
