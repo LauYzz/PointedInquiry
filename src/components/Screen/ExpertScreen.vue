150ee7ec8bbd07db8f358e7273267885458b5312
LauYzz <Liuyuanzhe2002@gmail.com>
2024-06-19 17:49:37 +0800

    update:home screen

--- src/components/Screen/ExpertScreen.vue
+++ src/components/Screen/ExpertScreen.vue
@@ -1,48 +1,81 @@
 <template>
   <div class="bg">
-    <link rel="stylesheet icon" href="//at.alicdn.com/t/c/font_3828548_f8oepiumx94.css">
-    <div style="display:flex;background-color: white;">
-      <el-input style="flex:8;border-color:rgb(11, 141, 130)" v-model="keywords"></el-input>
-      <el-button style="flex:1" type="primary" @click="search">搜索</el-button>
+    <link
+      rel="stylesheet icon"
+      href="//at.alicdn.com/t/c/font_3828548_f8oepiumx94.css"
+    />
+    <div style="display: flex; background-color: white">
+      <el-input
+        style="flex: 8; border-color: rgb(11, 141, 130)"
+        v-model="keywords"
+      ></el-input>
+      <el-button style="flex: 1" type="primary" @click="search">搜索</el-button>
     </div>
     <div class="icons">
-      <span class="com" :class="{active: selectedType === 1}" @click="changeView(1)">
+      <span
+        class="com"
+        :class="{ active: selectedType === 1 }"
+        @click="changeView(1)"
+      >
         <i class="iconfont icon-shangyehezuo-copy"></i>
-        <div style="text-align:center;">心理</div>
+        <div style="text-align: center">心理</div>
       </span>
-      <span class="com" :class="{active: selectedType === 2}" style="margin-left:50px;margin-right:50px;" @click="changeView(2)">
+      <span
+        class="com"
+        :class="{ active: selectedType === 2 }"
+        style="margin-left: 50px; margin-right: 50px"
+        @click="changeView(2)"
+      >
         <i class="iconfont icon-bianji_edit"></i>
-        <div style="text-align:center;">学业</div>
+        <div style="text-align: center">学业</div>
       </span>
-      <span class="com" :class="{active: selectedType === 3}" @click="changeView(3)">
+      <span
+        class="com"
+        :class="{ active: selectedType === 3 }"
+        @click="changeView(3)"
+      >
         <i class="iconfont icon-gongzuotai_workbench"></i>
-        <div style="text-align:center;">就业</div>
+        <div style="text-align: center">就业</div>
       </span>
     </div>
     <div class="sort-buttons">
       <el-button class="sort-button" @click="sortByRating">
         评分
-        <i class="el-icon-sort" style="margin-left: 5px;"></i>
+        <i class="el-icon-sort" style="margin-left: 5px"></i>
       </el-button>
       <el-button class="sort-button" @click="sortByReviews">
         评论量
-        <i class="el-icon-sort" style="margin-left: 5px;"></i>
+        <i class="el-icon-sort" style="margin-left: 5px"></i>
       </el-button>
     </div>
-    <el-card :body-style="{ padding: '0px' }" v-for="(item, index) in Experts" :key="index">
-      <div style="padding: 14px;" @click="linkToExpertDetail(item, index)">
+    <el-card
+      :body-style="{ padding: '0px' }"
+      v-for="(item, index) in Experts"
+      :key="index"
+    >
+      <div style="padding: 14px" @click="linkToExpertDetail(item, index)">
         <div>
           <span class="name">{{ item.realName }}&nbsp;&nbsp;</span>
           <span class="price">最低价格：{{ item.price }}元/小时</span>
-          <br>
+          <br />
           <span class="title">{{ item.job }}</span>
         </div>
-        <br>
-        <el-rate class="rate" v-model="item.rating" disabled show-score></el-rate>
+        <br />
+        <el-rate
+          class="rate"
+          v-model="item.rating"
+          disabled
+          show-score
+        ></el-rate>
         <div class="description">
           <div class="subdes">
-            <span class="topic" v-for="(subItem, subIndex) in item.topics" :key="subIndex">
-              <i style="color:white; font-size: 20px;">#&nbsp;</i>{{ subItem.title }}
+            <span
+              class="topic"
+              v-for="(subItem, subIndex) in item.topics"
+              :key="subIndex"
+            >
+              <i style="color: white; font-size: 20px">#&nbsp;</i
+              >{{ subItem.title }}
               <br v-if="subIndex < item.topics.length - 1" />
             </span>
             <span class="type">{{ getTypeText(item.type) }}</span>
@@ -56,9 +89,9 @@
 
 <script>
 import axios from "axios";
-import Expert from '../expert.vue';
-import Test4 from '../Test4.vue';
-import Test5 from '../Test5.vue';
+import Expert from "../expert.vue";
+import Test4 from "../Test4.vue";
+import Test5 from "../Test5.vue";
 
 export default {
   data() {
@@ -71,7 +104,7 @@ export default {
   components: {
     Expert,
     Test4,
-    Test5
+    Test5,
   },
   methods: {
     search() {
@@ -79,14 +112,16 @@ export default {
         url: "/expert/search",
         method: "get",
         params: {
-          content: this.keywords
-        }
+          content: this.keywords,
+        },
       };
-      axios(config).then((res) => {
-        this.Experts = res.data.data;
-      }).catch(error => {
-        console.log(error);
-      });
+      axios(config)
+        .then((res) => {
+          this.Experts = res.data.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
     getTypeText(type) {
       switch (type) {
@@ -118,18 +153,20 @@ export default {
         url: "/expert/getAllByRating",
         method: "get",
         params: {
-          type: this.selectedType
-        }
+          type: this.selectedType,
+        },
       };
-      axios(config).then((res) => {
-        if (res.data && res.data.data) {
-          this.Experts = res.data.data;
-        } else {
-          console.error("Unexpected response format:", res);
-        }
-      }).catch(error => {
-        console.log(error);
-      });
+      axios(config)
+        .then((res) => {
+          if (res.data && res.data.data) {
+            this.Experts = res.data.data;
+          } else {
+            console.error("Unexpected response format:", res);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
     sortByReviews() {
       console.log("sortByReviews clicked");
@@ -137,27 +174,29 @@ export default {
         url: "/expert/getAllByCommentCount",
         method: "get",
         params: {
-          type: this.selectedType
-        }
+          type: this.selectedType,
+        },
       };
-      axios(config).then((res) => {
-        if (res.data && res.data.data) {
-          // console.log("sortByReviews response:", res.data.data);
-          this.Experts = res.data.data;
-        } else {
-          console.error("Unexpected response format:", res);
-        }
-      }).catch(error => {
-        console.log(error);
-      });
+      axios(config)
+        .then((res) => {
+          if (res.data && res.data.data) {
+            // console.log("sortByReviews response:", res.data.data);
+            this.Experts = res.data.data;
+          } else {
+            console.error("Unexpected response format:", res);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
     async queryData() {
       const config = {
-        method: 'get',
-        url: '/expert/getAll',
+        method: "get",
+        url: "/expert/getAll",
         params: {
-          type: this.selectedType
-        }
+          type: this.selectedType,
+        },
       };
       const res = await axios(config);
       if (res.data && res.data.data) {
@@ -177,10 +216,10 @@ export default {
     async changeView(index) {
       this.selectedType = index;
       const config = {
-        url: '/expert/getAll',
+        url: "/expert/getAll",
         params: {
-          type: index
-        }
+          type: index,
+        },
       };
       axios(config).then((res) => {
         if (res.data && res.data.data) {
@@ -193,8 +232,8 @@ export default {
   },
   mounted() {
     this.queryData();
-  }
-}
+  },
+};
 </script>
 
 <style scoped lang="less">
@@ -317,9 +356,9 @@ export default {
 .el-card {
   position: relative;
   border-radius: 2px;
-  background-color: #FFF;
+  background-color: #fff;
   overflow: hidden;
-  transition: .3s;
+  transition: 0.3s;
   margin-top: 20px;
   height: auto;
 }
